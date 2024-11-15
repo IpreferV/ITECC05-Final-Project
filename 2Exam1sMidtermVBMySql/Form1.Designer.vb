d21ff12379f6b99000cea38611bd578a1095ef4d
Vault Air <IpreferV@users.noreply.github.com>
2024-11-15 13:04:43 +0800

    Project.

--- 2Exam1sMidtermVBMySql/Form1.Designer.vb
+++ 2Exam1sMidtermVBMySql/Form1.Designer.vb
@@ -22,16 +22,91 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        btn_Test = New Button()
+        txtBox_userName = New TextBox()
+        Label1 = New Label()
+        txtBox_password = New TextBox()
+        Label2 = New Label()
+        btn_Login = New Button()
         SuspendLayout()
+        ' 
+        ' btn_Test
+        ' 
+        btn_Test.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_Test.Location = New Point(12, 12)
+        btn_Test.Name = "btn_Test"
+        btn_Test.Size = New Size(269, 53)
+        btn_Test.TabIndex = 0
+        btn_Test.Text = "Test Connection"
+        btn_Test.UseVisualStyleBackColor = True
+        ' 
+        ' txtBox_userName
+        ' 
+        txtBox_userName.Location = New Point(99, 101)
+        txtBox_userName.Name = "txtBox_userName"
+        txtBox_userName.Size = New Size(182, 23)
+        txtBox_userName.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(12, 99)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(81, 21)
+        Label1.TabIndex = 2
+        Label1.Text = "Username"
+        ' 
+        ' txtBox_password
+        ' 
+        txtBox_password.Location = New Point(99, 157)
+        txtBox_password.Name = "txtBox_password"
+        txtBox_password.PasswordChar = "*"c
+        txtBox_password.Size = New Size(182, 23)
+        txtBox_password.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(12, 155)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(76, 21)
+        Label2.TabIndex = 2
+        Label2.Text = "Password"
+        ' 
+        ' btn_Login
+        ' 
+        btn_Login.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btn_Login.Location = New Point(206, 186)
+        btn_Login.Name = "btn_Login"
+        btn_Login.Size = New Size(75, 37)
+        btn_Login.TabIndex = 4
+        btn_Login.Text = "Login"
+        btn_Login.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(btn_Login)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtBox_password)
+        Controls.Add(txtBox_userName)
+        Controls.Add(btn_Test)
         Name = "Form1"
         Text = "Midterm Examination VB"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents btn_Test As Button
+    Friend WithEvents txtBox_userName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtBox_password As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btn_Login As Button
 
 End Class
--- 2Exam1sMidtermVBMySql/Form1.vb
+++ 2Exam1sMidtermVBMySql/Form1.vb
@@ -1,3 +1,52 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
 
+Public Class Form1
+    Dim MySqlConn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+
+    Private Sub btn_Test_Click(sender As Object, e As EventArgs) Handles btn_Test.Click
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=localhost;userid=root;password='';database=usercredentials"
+
+        Try
+            MySqlConn.Open()
+            MessageBox.Show("Success!", "Testing UserCredentials DB Connection")
+            MySqlConn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            MySqlConn.Close()
+        End Try
+    End Sub
+
+    Private Sub btn_Login_Click(sender As Object, e As EventArgs) Handles btn_Login.Click
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=localhost;userid=root;password='';database=usercredentials"
+        Dim READER As MySqlDataReader
+
+        Try
+            MySqlConn.Open()
+            Dim Query = "SELECT * FROM usercredentials.user_data WHERE userName = '" & txtBox_userName.Text & "' AND userPassword = '" & txtBox_password.Text & "'"
+            COMMAND = New MySqlCommand(Query, MySqlConn)
+            READER = COMMAND.ExecuteReader
+            Dim count As Integer
+            count = 0
+
+            While READER.Read
+                count += 1
+            End While
+
+            If count = 1 Then
+                MessageBox.Show("Logged in successfully.")
+            ElseIf count > 1 Then
+                MessageBox.Show("Duplicate credential found.")
+            Else
+                MessageBox.Show("Invalid credential.")
+            End If
+
+            MySqlConn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            MySqlConn.Close()
+        End Try
+    End Sub
 End Class
