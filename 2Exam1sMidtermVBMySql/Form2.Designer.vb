36513254a518c98479e413e9ba11f47b5f2a4050
Vault Air <IpreferV@users.noreply.github.com>
2024-12-22 18:43:11 +0800

    Everything, Everywhere, All at Once

--- 2Exam1sMidtermVBMySql/Form2.Designer.vb
+++ 2Exam1sMidtermVBMySql/Form2.Designer.vb
@@ -22,6 +22,9 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New DataVisualization.Charting.Series()
         btn_SignOut = New Button()
         Label1 = New Label()
         txtBox_eID = New TextBox()
@@ -34,14 +37,28 @@ Partial Class Form2
         btn_Save = New Button()
         btn_Update = New Button()
         btn_Delete = New Button()
+        ComboBox1 = New ComboBox()
+        ListBox1 = New ListBox()
+        DataGridView1 = New DataGridView()
+        btn_LoadTable = New Button()
+        txt_Search = New TextBox()
+        Chart1 = New DataVisualization.Charting.Chart()
+        btn_LoadChart = New Button()
+        btn_rad_M = New RadioButton()
+        btn_rad_F = New RadioButton()
+        DateTimePicker1 = New DateTimePicker()
+        Label5 = New Label()
+        Label6 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(Chart1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' btn_SignOut
         ' 
         btn_SignOut.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        btn_SignOut.Location = New Point(13, 6)
+        btn_SignOut.Location = New Point(12, 6)
         btn_SignOut.Name = "btn_SignOut"
-        btn_SignOut.Size = New Size(92, 28)
+        btn_SignOut.Size = New Size(70, 28)
         btn_SignOut.TabIndex = 1
         btn_SignOut.Text = "Sign Out"
         btn_SignOut.UseVisualStyleBackColor = True
@@ -50,7 +67,7 @@ Partial Class Form2
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label1.Location = New Point(13, 66)
+        Label1.Location = New Point(13, 60)
         Label1.Name = "Label1"
         Label1.Size = New Size(32, 15)
         Label1.TabIndex = 2
@@ -58,7 +75,7 @@ Partial Class Form2
         ' 
         ' txtBox_eID
         ' 
-        txtBox_eID.Location = New Point(78, 63)
+        txtBox_eID.Location = New Point(78, 57)
         txtBox_eID.Name = "txtBox_eID"
         txtBox_eID.Size = New Size(138, 23)
         txtBox_eID.TabIndex = 3
@@ -67,7 +84,7 @@ Partial Class Form2
         ' 
         Label2.AutoSize = True
         Label2.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label2.Location = New Point(13, 100)
+        Label2.Location = New Point(13, 94)
         Label2.Name = "Label2"
         Label2.Size = New Size(40, 15)
         Label2.TabIndex = 2
@@ -77,7 +94,7 @@ Partial Class Form2
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label3.Location = New Point(13, 132)
+        Label3.Location = New Point(13, 126)
         Label3.Name = "Label3"
         Label3.Size = New Size(57, 15)
         Label3.TabIndex = 2
@@ -87,7 +104,7 @@ Partial Class Form2
         ' 
         Label4.AutoSize = True
         Label4.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        Label4.Location = New Point(12, 163)
+        Label4.Location = New Point(12, 157)
         Label4.Name = "Label4"
         Label4.Size = New Size(29, 15)
         Label4.TabIndex = 2
@@ -95,21 +112,21 @@ Partial Class Form2
         ' 
         ' txtBox_Name
         ' 
-        txtBox_Name.Location = New Point(78, 94)
+        txtBox_Name.Location = New Point(78, 88)
         txtBox_Name.Name = "txtBox_Name"
         txtBox_Name.Size = New Size(138, 23)
         txtBox_Name.TabIndex = 3
         ' 
         ' txtBox_Surname
         ' 
-        txtBox_Surname.Location = New Point(78, 129)
+        txtBox_Surname.Location = New Point(78, 123)
         txtBox_Surname.Name = "txtBox_Surname"
         txtBox_Surname.Size = New Size(138, 23)
         txtBox_Surname.TabIndex = 3
         ' 
         ' txtBox_Age
         ' 
-        txtBox_Age.Location = New Point(78, 163)
+        txtBox_Age.Location = New Point(78, 157)
         txtBox_Age.Name = "txtBox_Age"
         txtBox_Age.Size = New Size(138, 23)
         txtBox_Age.TabIndex = 3
@@ -119,7 +136,7 @@ Partial Class Form2
         btn_Save.BackColor = Color.PaleGreen
         btn_Save.FlatStyle = FlatStyle.Flat
         btn_Save.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        btn_Save.Location = New Point(13, 201)
+        btn_Save.Location = New Point(23, 273)
         btn_Save.Name = "btn_Save"
         btn_Save.Size = New Size(92, 28)
         btn_Save.TabIndex = 1
@@ -131,7 +148,7 @@ Partial Class Form2
         btn_Update.BackColor = Color.LemonChiffon
         btn_Update.FlatStyle = FlatStyle.Flat
         btn_Update.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        btn_Update.Location = New Point(111, 201)
+        btn_Update.Location = New Point(121, 273)
         btn_Update.Name = "btn_Update"
         btn_Update.Size = New Size(92, 28)
         btn_Update.TabIndex = 1
@@ -143,23 +160,159 @@ Partial Class Form2
         btn_Delete.BackColor = Color.LightCoral
         btn_Delete.FlatStyle = FlatStyle.Flat
         btn_Delete.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        btn_Delete.Location = New Point(13, 235)
+        btn_Delete.Location = New Point(23, 307)
         btn_Delete.Name = "btn_Delete"
         btn_Delete.Size = New Size(92, 28)
         btn_Delete.TabIndex = 1
         btn_Delete.Text = "Delete"
         btn_Delete.UseVisualStyleBackColor = False
         ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.AllowDrop = True
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(88, 10)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(128, 23)
+        ComboBox1.TabIndex = 4
+        ' 
+        ' ListBox1
+        ' 
+        ListBox1.FormattingEnabled = True
+        ListBox1.ItemHeight = 15
+        ListBox1.Location = New Point(222, 12)
+        ListBox1.Name = "ListBox1"
+        ListBox1.Size = New Size(164, 19)
+        ListBox1.TabIndex = 5
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(232, 59)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.Size = New Size(554, 164)
+        DataGridView1.TabIndex = 6
+        ' 
+        ' btn_LoadTable
+        ' 
+        btn_LoadTable.FlatStyle = FlatStyle.Flat
+        btn_LoadTable.Location = New Point(487, 13)
+        btn_LoadTable.Name = "btn_LoadTable"
+        btn_LoadTable.Size = New Size(92, 28)
+        btn_LoadTable.TabIndex = 7
+        btn_LoadTable.Text = "Load Table"
+        btn_LoadTable.UseVisualStyleBackColor = True
+        ' 
+        ' txt_Search
+        ' 
+        txt_Search.BorderStyle = BorderStyle.FixedSingle
+        txt_Search.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        txt_Search.Location = New Point(585, 12)
+        txt_Search.Name = "txt_Search"
+        txt_Search.Size = New Size(201, 29)
+        txt_Search.TabIndex = 8
+        ' 
+        ' Chart1
+        ' 
+        ChartArea1.AxisX.Interval = 1R
+        ChartArea1.AxisX.IsLabelAutoFit = False
+        ChartArea1.AxisX.LabelStyle.Angle = -90
+        ChartArea1.Name = "ChartArea1"
+        Chart1.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Chart1.Legends.Add(Legend1)
+        Chart1.Location = New Point(232, 229)
+        Chart1.Name = "Chart1"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Name vs. Age"
+        Chart1.Series.Add(Series1)
+        Chart1.Size = New Size(554, 183)
+        Chart1.TabIndex = 9
+        Chart1.Text = "Chart1"
+        ' 
+        ' btn_LoadChart
+        ' 
+        btn_LoadChart.FlatStyle = FlatStyle.Flat
+        btn_LoadChart.Location = New Point(121, 307)
+        btn_LoadChart.Name = "btn_LoadChart"
+        btn_LoadChart.Size = New Size(92, 28)
+        btn_LoadChart.TabIndex = 7
+        btn_LoadChart.Text = "Load Chart"
+        btn_LoadChart.UseVisualStyleBackColor = True
+        ' 
+        ' btn_rad_M
+        ' 
+        btn_rad_M.AutoSize = True
+        btn_rad_M.Location = New Point(76, 220)
+        btn_rad_M.Name = "btn_rad_M"
+        btn_rad_M.Size = New Size(51, 19)
+        btn_rad_M.TabIndex = 10
+        btn_rad_M.TabStop = True
+        btn_rad_M.Text = "Male"
+        btn_rad_M.UseVisualStyleBackColor = True
+        ' 
+        ' btn_rad_F
+        ' 
+        btn_rad_F.AutoSize = True
+        btn_rad_F.Location = New Point(133, 220)
+        btn_rad_F.Name = "btn_rad_F"
+        btn_rad_F.Size = New Size(63, 19)
+        btn_rad_F.TabIndex = 10
+        btn_rad_F.TabStop = True
+        btn_rad_F.Text = "Female"
+        btn_rad_F.UseVisualStyleBackColor = True
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.CustomFormat = "YYYY-MM-DD"
+        DateTimePicker1.Format = DateTimePickerFormat.Short
+        DateTimePicker1.Location = New Point(78, 186)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(138, 23)
+        DateTimePicker1.TabIndex = 11
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(15, 191)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(33, 15)
+        Label5.TabIndex = 2
+        Label5.Text = "DOB"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(15, 220)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(49, 15)
+        Label6.TabIndex = 2
+        Label6.Text = "Gender"
+        ' 
         ' Form2
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(253, 295)
-        ControlBox = False
+        ClientSize = New Size(798, 429)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(btn_rad_F)
+        Controls.Add(btn_rad_M)
+        Controls.Add(Chart1)
+        Controls.Add(txt_Search)
+        Controls.Add(btn_LoadChart)
+        Controls.Add(btn_LoadTable)
+        Controls.Add(DataGridView1)
+        Controls.Add(ListBox1)
+        Controls.Add(ComboBox1)
         Controls.Add(txtBox_Age)
         Controls.Add(txtBox_Surname)
         Controls.Add(txtBox_Name)
         Controls.Add(txtBox_eID)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
         Controls.Add(Label4)
         Controls.Add(Label3)
         Controls.Add(Label2)
@@ -168,9 +321,13 @@ Partial Class Form2
         Controls.Add(btn_Update)
         Controls.Add(btn_Save)
         Controls.Add(btn_SignOut)
+        MaximizeBox = False
+        MinimizeBox = False
         Name = "Form2"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Data"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(Chart1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -186,4 +343,16 @@ Partial Class Form2
     Friend WithEvents btn_Save As Button
     Friend WithEvents btn_Update As Button
     Friend WithEvents btn_Delete As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btn_LoadTable As Button
+    Friend WithEvents txt_Search As TextBox
+    Friend WithEvents Chart1 As DataVisualization.Charting.Chart
+    Friend WithEvents btn_LoadChart As Button
+    Friend WithEvents btn_rad_M As RadioButton
+    Friend WithEvents btn_rad_F As RadioButton
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
 End Class
--- 2Exam1sMidtermVBMySql/Form2.vb
+++ 2Exam1sMidtermVBMySql/Form2.vb
@@ -2,8 +2,10 @@
 Imports Mysqlx.Crud
 
 Public Class Form2
+    Dim gender As String
     Dim MySqlConn As MySqlConnection
     Dim COMMAND As MySqlCommand
+    Dim dbDataSet As New DataTable
     Private Sub btn_SignOut_Click(sender As Object, e As EventArgs) Handles btn_SignOut.Click
         Form1.Show()
         Me.Hide()
@@ -16,7 +18,7 @@ Public Class Form2
 
         Try
             MySqlConn.Open()
-            Dim Query = "INSERT INTO itecc05DB.eData (eID, name, surname, age) VALUES ('" & txtBox_eID.Text & "', '" & txtBox_Name.Text & "', '" & txtBox_Surname.Text & "', '" & txtBox_Age.Text & "')"
+            Dim Query = "INSERT INTO itecc05DB.eData (eID, name, surname, age, gender, DOB) VALUES ('" & txtBox_eID.Text & "', '" & txtBox_Name.Text & "', '" & txtBox_Surname.Text & "', '" & txtBox_Age.Text & "', '" & gender & "', '" & DateTimePicker1.Text & "')"
             COMMAND = New MySqlCommand(Query, MySqlConn)
             READER = COMMAND.ExecuteReader
 
@@ -30,6 +32,8 @@ Public Class Form2
         Finally
             MySqlConn.Dispose()
         End Try
+
+        LoadTable()
     End Sub
 
     Private Sub btn_Update_Click(sender As Object, e As EventArgs) Handles btn_Update.Click
@@ -39,7 +43,7 @@ Public Class Form2
 
         Try
             MySqlConn.Open()
-            Dim Query = "UPDATE itecc05DB.eData SET eid='" & txtBox_eID.Text & "', name='" & txtBox_Name.Text & "', surname='" & txtBox_Surname.Text & "', age='" & txtBox_Age.Text & "' WHERE eid = '" & txtBox_eID.Text & "'"
+            Dim Query = "UPDATE itecc05DB.eData SET eid='" & txtBox_eID.Text & "', name='" & txtBox_Name.Text & "', surname='" & txtBox_Surname.Text & "', age='" & txtBox_Age.Text & "', gender='" & gender & "', dob='" & DateTimePicker1.Text & "' WHERE eid = '" & txtBox_eID.Text & "'"
             COMMAND = New MySqlCommand(Query, MySqlConn)
             READER = COMMAND.ExecuteReader
 
@@ -53,6 +57,8 @@ Public Class Form2
         Finally
             MySqlConn.Dispose()
         End Try
+
+        LoadTable()
     End Sub
 
     Private Sub btn_Delete_Click(sender As Object, e As EventArgs) Handles btn_Delete.Click
@@ -76,5 +82,200 @@ Public Class Form2
         Finally
             MySqlConn.Dispose()
         End Try
+
+        LoadTable()
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadTable()
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=localhost;userid=root;password='';database=itecc05DB"
+        Dim READER As MySqlDataReader
+
+        Try
+            MySqlConn.Open()
+            Dim Query = "SELECT * FROM itecc05DB.edata"
+            COMMAND = New MySqlCommand(Query, MySqlConn)
+            READER = COMMAND.ExecuteReader
+            While READER.Read
+                Dim sName = READER.GetString("name")
+                ComboBox1.Items.Add(sName)
+                ListBox1.Items.Add(sName)
+            End While
+
+            MySqlConn.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            MySqlConn.Close()
+        Finally
+            MySqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=localhost;userid=root;password='';database=itecc05DB"
+        Dim READER As MySqlDataReader
+
+        Try
+            MySqlConn.Open()
+            Dim Query = "SELECT * FROM itecc05DB.edata WHERE name = '" & ComboBox1.Text & "'"
+            COMMAND = New MySqlCommand(Query, MySqlConn)
+            READER = COMMAND.ExecuteReader
+            While READER.Read
+                txtBox_eID.Text = READER.GetInt32("eID")
+                txtBox_Name.Text = READER.GetString("name")
+                txtBox_Surname.Text = READER.GetString("surname")
+                txtBox_Age.Text = READER.GetInt32("eID")
+            End While
+
+
+            MySqlConn.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            MySqlConn.Close()
+        Finally
+            MySqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=localhost;userid=root;password='';database=itecc05DB"
+        Dim READER As MySqlDataReader
+
+        Try
+            MySqlConn.Open()
+            Dim Query = "SELECT * FROM itecc05DB.edata WHERE name = '" & ListBox1.Text & "'"
+            COMMAND = New MySqlCommand(Query, MySqlConn)
+            READER = COMMAND.ExecuteReader
+            While READER.Read
+                txtBox_eID.Text = READER.GetInt32("eID")
+                txtBox_Name.Text = READER.GetString("name")
+                txtBox_Surname.Text = READER.GetString("surname")
+                txtBox_Age.Text = READER.GetInt32("eID")
+            End While
+
+
+            MySqlConn.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            MySqlConn.Close()
+        Finally
+            MySqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub LoadTable()
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=localhost;userid=root;password='';database=itecc05DB"
+        Dim SDA As New MySqlDataAdapter
+        Dim bSource As New BindingSource
+
+        Try
+            MySqlConn.Open()
+            Dim Query = "SELECT eid as 'Employee ID', name as 'First Name', surname as 'Last Name', age as 'Age', gender as 'Gender', dob as 'Date of Birth' FROM itecc05DB.edata"
+            COMMAND = New MySqlCommand(Query, MySqlConn)
+            SDA.SelectCommand = COMMAND
+            SDA.Fill(dbDataSet)
+            bSource.DataSource = dbDataSet
+            DataGridView1.DataSource = bSource
+            SDA.Update(dbDataSet)
+
+            MySqlConn.Close()
+
+        Catch ex As MySqlException
+            MsgBox(ex.Message)
+            MySqlConn.Close()
+        Finally
+            MySqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub btn_LoadTable_Click(sender As Object, e As EventArgs) Handles btn_LoadTable.Click
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=localhost;userid=root;password='';database=itecc05DB"
+        Dim SDA As New MySqlDataAdapter
+        Dim dbDataSet As New DataTable
+        Dim bSource As New BindingSource
+
+        Try
+            MySqlConn.Open()
+            Dim Query = "SELECT eid as 'Employee ID', name as 'First Name', surname as 'Last Name', age as 'Age', gender as 'Gender', dob as 'Date of Birth' FROM itecc05DB.edata"
+            COMMAND = New MySqlCommand(Query, MySqlConn)
+            SDA.SelectCommand = COMMAND
+            SDA.Fill(dbDataSet)
+            bSource.DataSource = dbDataSet
+            DataGridView1.DataSource = bSource
+            SDA.Update(dbDataSet)
+
+            MySqlConn.Close()
+
+        Catch ex As MySqlException
+            MsgBox(ex.Message)
+            MySqlConn.Close()
+        Finally
+            MySqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub btn_LoadChart_Click(sender As Object, e As EventArgs) Handles btn_LoadChart.Click
+        MySqlConn = New MySqlConnection
+        MySqlConn.ConnectionString = "server=localhost;userid=root;password='';database=itecc05DB"
+        Dim READER As MySqlDataReader
+
+        Try
+            MySqlConn.Open()
+            Dim Query = "SELECT * FROM itecc05DB.edata"
+            COMMAND = New MySqlCommand(Query, MySqlConn)
+            READER = COMMAND.ExecuteReader
+            While READER.Read
+                Chart1.Series("Name vs. Age").Points.AddXY(READER.GetInt32("eid"), READER.GetInt32("age"))
+            End While
+
+        Catch ex As MySqlException
+            MySqlConn.Close()
+        Finally
+            MySqlConn.Dispose()
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow
+            row = Me.DataGridView1.Rows(e.RowIndex)
+
+            txtBox_eID.Text = row.Cells("Employee ID").Value.ToString
+            txtBox_Name.Text = row.Cells("First Name").Value.ToString
+            txtBox_Surname.Text = row.Cells("Last Name").Value.ToString
+            txtBox_Age.Text = row.Cells("Age").Value.ToString
+        End If
+    End Sub
+
+    Private Sub txt_Search_TextChanged(sender As Object, e As EventArgs) Handles txt_Search.TextChanged
+        Dim DV As New DataView(dbDataSet)
+        DV.RowFilter = String.Format("[First Name] LIKE '%{0}%'", txt_Search.Text)
+        DataGridView1.DataSource = DV
+    End Sub
+
+    Private Sub Form2_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim dialogue As DialogResult
+        dialogue = MessageBox.Show("Do you want to exit the application?", "Exit", MessageBoxButtons.YesNo)
+        If dialogue = DialogResult.No Then
+            e.Cancel = True
+        Else
+            Application.ExitThread()
+        End If
+    End Sub
+
+    Private Sub btn_rad_M_CheckedChanged(sender As Object, e As EventArgs) Handles btn_rad_M.CheckedChanged
+        gender = "Male"
+    End Sub
+
+    Private Sub btn_rad_F_CheckedChanged(sender As Object, e As EventArgs) Handles btn_rad_F.CheckedChanged
+        gender = "Female"
     End Sub
 End Class
